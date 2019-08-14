8c9724e5e7af0ba8f2723df3bd244774e43dfac5
soapsnake <soapsnake@gmail.com>
2019-08-14 23:24:13 +0800

    fix

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # start learn scala and spark
+
+
+
+[scala learning guide](https://docs.scala-lang.org/tour/basics.html)
--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,15 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("hello world!!!")
+
+
+
+
+    val x = 1 + 1
+
+    println(x)
+
+//    x = 3    //val 声明的变量不能被重新赋值
+//    println(x)
   }
 }
